f5f02f1700b17bccf66e3ae8f25bc6d8218cbd70
SOLLETIRAHUL23 <rahulsolleti46@gmail.com>
2025-05-21 18:36:25 -0500

    Created user login page with session Id

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Editor = () => {
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/CodeCoLab.png.jpg" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
+      </div>
+      <div className="editorWrap">
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
